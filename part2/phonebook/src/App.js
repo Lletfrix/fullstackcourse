e27a51cd2636499a708f1a997b7585a63f5eb36d
lletfrix <lletfrix@gmail.com>
2019-07-08 21:02:57 +0200

    phonebook partc

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,13 @@
-import React, { useState } from 'react'
-import Contacts from './components/Contacts'
-import Filter from './components/Filter'
-import Form from './components/Form'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Contacts from './components/Contacts';
+import Filter from './components/Filter';
+import Form from './components/Form';
 
 const App = () => {
     // Variables and state:
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]);
+    const [persons, setPersons] = useState([]);
     const [ newName, setNewName ] = useState('');
     const [ newPhone, setNewPhone ] = useState('');
     const [ filter, setFilter ] = useState('');
@@ -31,7 +28,14 @@ const App = () => {
         setNewName('');
         setNewPhone('');
     };
-    
+
+    //Fetch data from sv
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => setPersons(response.data));
+    },[]);
+
     // Rendering
     return (
         <div>
