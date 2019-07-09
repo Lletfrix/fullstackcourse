4448ac529566f62c31c0518fceb292f06a9e97b6
Lletfrix <lletfrix@gmail.com>
2019-07-09 13:35:00 +0200

    datacountries

--- part2/datacountries/src/App.js
+++ part2/datacountries/src/App.js
@@ -1,31 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const Country = ({country}) => {
+const Weather = ({capital}) => {
+    //State
+    const [info, setInfo] = useState({temp_c:0, icon:'', wind:''});
+    //Fetch
+    useEffect(() => {
+        axios
+            .get('http://api.apixu.com/v1/current.json?key=d7b966bd0ca6455abd8104353190907&q='+capital)
+            .then(response =>{
+                const data = response.data.current;
+                const newInfo = {
+                    temp_c: data.temp_c,
+                    icon: data.condition.icon,
+                    wind: data.wind_kph + ' direction ' + data.wind_dir
+                };
+                setInfo(newInfo);
+            });
+    }, [capital]);
+    //Render
+    return(
+        <div>
+            <strong>temperature:</strong> {info.temp_c} Celsius
+            <br/>
+            <img alt='weather icon' src={info.icon}/>
+            <br/>
+            <strong>wind:</strong> {info.wind}
+        </div>
+    );
+}
+
+const Country = ({country, unique=false}) => {
+    //State
     const [ showMore, setShowMore ] = useState(false);
-    if (showMore){
+    //Render conditionally
+    if (showMore !== unique){
         return(
             <div>
-            <h1>{country.name}</h1>
-            <button onClick={() => setShowMore(!showMore)}>{showMore ? 'close' : 'show' }</button>
-            <div>
-                capital {country.capital}
-                <br/>
-                population {country.population}
+                <h1>{country.name}</h1>
+                <button onClick={() => setShowMore(!showMore)}>{showMore !== unique ? 'close' : 'show' }</button>
+                <div>
+                    capital {country.capital}
+                    <br/>
+                    population {country.population}
+                </div>
+                <h2>languages</h2>
+                <ul>
+                    {country.languages.map(lang => <li key={lang.iso639_1}>{lang.name}</li>)}
+                </ul>
+                <img src={country.flag} alt='flag' height='90 pt'/>
+                <div>
+                    <h2>Weather in {country.capital}</h2>
+                    <Weather capital={country.capital}/>
+                </div>
             </div>
-            <h2>languages</h2>
-            <ul>
-                {country.languages.map(lang => <li key={lang.iso639_1}>{lang.name}</li>)}
-            </ul>
-            <img src={country.flag} alt='flag' height='90 pt'/>
-        </div>
         );
     }
 
     return (
         <div>
             {country.name}
-            <button onClick={() => setShowMore(!showMore)}>{showMore ? 'close' : 'show' }</button>
+            <button onClick={() => setShowMore(!showMore)}>{showMore !== unique ? 'close' : 'show' }</button>
         </div>
     );
 };
@@ -38,7 +73,7 @@ const Countries = ({countries, input}) =>{
     }else if (countries.length === 0){
         return (<p> There are no countries to show.</p>);
     }else if (countries.length === 1){
-        return <Country country={countries[0]}/>;
+        return <Country country={countries[0]} unique={true}/>;
     }
 
     return (
